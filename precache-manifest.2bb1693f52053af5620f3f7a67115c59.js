578dc3ab66420fedd7aa4ee671c5e45054945b4a
ikarampinis <yianniskarabinis@gmail.com>
2021-05-22 20:38:52 +0300

    Updates

---
+++ precache-manifest.2bb1693f52053af5620f3f7a67115c59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc260342974e6a0500297d772da53fa5",
+    "url": "/auction-app/index.html"
+  },
+  {
+    "revision": "7f35a8961444d3856676",
+    "url": "/auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0d9e28155938b98274ff",
+    "url": "/auction-app/static/js/2.95a0e623.chunk.js"
+  },
+  {
+    "revision": "7f35a8961444d3856676",
+    "url": "/auction-app/static/js/main.1d82a5de.chunk.js"
+  },
+  {
+    "revision": "f3bf5b93f7e7136135cd",
+    "url": "/auction-app/static/js/runtime-main.ad8c3a94.js"
+  }
+]);
